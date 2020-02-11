c0280ce01eb7ec470db050dd925f789c3b62b3b2
Lars Thorup <lars@zealake.com>
2020-02-11 18:09:48 +0100

    merge, shallow equality friendly, slow

---
+++ src/index.ts
@@ -0,0 +1,94 @@
+import * as assert from 'assert';
+import * as R from 'ramda';
+
+type Address = {
+  street: string;
+  zip: number;
+};
+
+type Person = {
+  id: string;
+  name: string;
+  address: Address;
+};
+
+type People = { [id: string]: Person };
+
+type State = {
+  people: People;
+};
+
+type PartialPeople = { [id: string]: Person | null };
+
+type PartialState = {
+  people: PartialPeople;
+};
+
+function mergeState(state: State, payload: PartialState) {
+  const newState: State = R.mergeDeepRight(state, payload);
+  return R.equals(state, newState) ? state : newState;
+}
+
+const initialState: State = {
+  people: {
+    '1': {
+      id: '1',
+      name: 'Lars',
+      address: {
+        street: 'Hovedgaden',
+        zip: 3460,
+      },
+    },
+  },
+};
+
+const addPerson: PartialState = {
+  people: {
+    '2': {
+      id: '2',
+      name: 'Hanne',
+      address: {
+        street: 'Køgemestervej',
+        zip: 3400,
+      },
+    },
+  },
+};
+
+const updatePerson: PartialState = {
+  people: {
+    '2': {
+      id: '2',
+      name: 'Jens',
+      address: {
+        street: 'Køgemestervej',
+        zip: 3400,
+      },
+    },
+  },
+};
+
+const deletePerson: PartialState = {
+  people: {
+    '2': null,
+  },
+};
+
+const state = mergeState(initialState, addPerson);
+assert.equal(mergeState(state, addPerson), state);
+assert.notEqual(mergeState(state, updatePerson), state);
+assert.equal(mergeState(state, updatePerson).people['2'].name, 'Jens');
+assert.ok(R.equals(mergeState(state, deletePerson), {
+  people: {
+    '1': {
+      id: '1',
+      name: 'Lars',
+      address: {
+        street: 'Hovedgaden',
+        zip: 3460,
+      },
+    },
+    '2': null
+  },
+}));
+console.log('done');
